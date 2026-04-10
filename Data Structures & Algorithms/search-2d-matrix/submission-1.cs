895a2f5fd197e2aed4cecd954a8dea6982a67ce0
Carson <77589065+nosra@users.noreply.github.com>
2026-04-10 15:06:57 -0400

    Add: search-2d-matrix - submission-1

---
+++ Data Structures & Algorithms/search-2d-matrix/submission-1.cs
@@ -0,0 +1,23 @@
+public class Solution {
+    public bool SearchMatrix(int[][] matrix, int target) {
+        // 2d matrices can be flattened down into 1d matrices pretty easily.
+        int left = 0;
+        int right = matrix.Length * matrix[0].Length - 1;
+        while(left <= right)
+        {
+            int mid = left + (right - left) / 2;
+            int midRow = mid / matrix[0].Length;
+            int midCol = mid % matrix[0].Length;
+            //Console.WriteLine($"mid: {mid}");
+            //Console.WriteLine($"{midRow}, {midCol}");
+            if(matrix[midRow][midCol] == target)
+                return true;
+            // on right
+            if(matrix[midRow][midCol] < target)
+                left = mid + 1;
+            else
+                right = mid - 1;
+        }
+        return false;
+    }
+}
